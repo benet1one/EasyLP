dfde55e27c681c4b535753b97f294396dace254c
benet1one <benet1one@gmail.com>
2024-05-12 08:05:28 +0200

    Initial commit

---
+++ R/objects.R
@@ -0,0 +1,23 @@
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+require(rlang)
+
+#' Create a Linear Program Object using expressions
+#'
+#' @param objective Objective function.
+#' @param ... Constraints
+#'
+#' @return
+#' @export
+#'
+#' @examples
+visualp <- function(objective, ...) {
+
+}
+
+
+
+
+
